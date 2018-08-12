f7a14d232451f4d2952b0e069261ee526e12a047
goodsk8er11@yahoo.com <monica@thenugbug.com>
2018-08-12 17:56:23 -0400

    Added settings and sort. Online music sort works right now

--- app/src/main/java/com/example/android/thenewsapp/MainActivity.java
+++ app/src/main/java/com/example/android/thenewsapp/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.android.thenewsapp;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -21,6 +25,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements android.support.v4.app.LoaderManager.LoaderCallbacks<List<News>> {
     private NewsAdapter mAdapter;
     private TextView mNoContentTextView;
+
+    public static String REQUEST_URL =
+            "https://content.guardianapis.com/search?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +64,19 @@ public class MainActivity extends AppCompatActivity implements android.support.v
 
     @Override
     public Loader<List<News>> onCreateLoader(int id, Bundle args) {
-        return new NewsLoader(this);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("api-key", "50624373-cd3c-406c-99e3-58d16c6a95ae");
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("section", orderBy);
+
+        return new NewsLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -75,5 +94,24 @@ public class MainActivity extends AppCompatActivity implements android.support.v
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
         mAdapter.clear();
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/android/thenewsapp/NewsLoader.java
+++ app/src/main/java/com/example/android/thenewsapp/NewsLoader.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 public class NewsLoader extends AsyncTaskLoader<List<News>> {
 
-    private static String REQUEST_URL =
-            "https://content.guardianapis.com/search?show-tags=contributor&api-key=50624373-cd3c-406c-99e3-58d16c6a95ae";
+    public static String s ;
 
-    public NewsLoader(Context context) {
+    public NewsLoader(Context context, String url) {
         super(context);
+        this.s = url;
     }
 
     @Override
@@ -21,11 +21,11 @@ public class NewsLoader extends AsyncTaskLoader<List<News>> {
 
     @Override
     public List<News> loadInBackground() {
-        if (REQUEST_URL == null) {
+        if (s == null) {
             return null;
         }
 
-        List<News> newsList = QueryUtils.fetchNewsData(REQUEST_URL);
+        List<News> newsList = QueryUtils.fetchNewsData(s);
         return newsList;
     }
 }
